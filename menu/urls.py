8149f57b99424362ef1ecf15ed3ce6d076a51fad
kubanuch03 <zazaka71@gmail.com>
2023-06-11 15:42:46 +0600

    11.06.23

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,10 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.get_index,name = 'index'),
+    path('about/', views.about,name = 'about'),
+    path('elements/', views.elements,name = 'elements'),
+    path('contact/', views.contact,name = 'contact'),
+    path('single-blog/', views.single_blog,name = 'single_blog'),
+    path('blog/', views.blog,name = 'blog'),
     
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
 def get_index(request):
-    return render(request,'index.html',locals())
+    return render(request,'menu_food/index.html',locals())
+
+def about(request):
+    return render(request,'menu_food/index.html',locals())
+
+def elements(request):
+    return render(request,'menu_food/elements.html',locals())
+
+def contact(request):
+    return render(request,'menu_food/contact.html',locals())
+
+def blog(request):
+    return render(request,'menu_food/blog.html',locals())
+
+def single_blog(request):
+    return render(request,'menu_food/single-blog.html',locals())
